43103e062e16b5b608c2883a102db523b0fd813c
AnnaKitou <93220476+AnnaKitou@users.noreply.github.com>
2022-01-25 13:58:19 +0200

    Add files via upload

---
+++ Kitou_School_Project/Kitou_School_Project/Controller/CourseController.cs
@@ -0,0 +1,48 @@
+﻿using Kitou_School_Project.RepositoryService;
+using Kitou_School_Project.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kitou_School_Project.Controller
+{
+    class CourseController
+    {
+        CourseRepository courseService = new CourseRepository();
+        StudentRepository studentService = new StudentRepository();
+        public void ReadCourses()
+        {
+            var courses = courseService.GetAll();
+            CourseView.PrintCourses(courses);
+        }
+        public void ReadAllStudentsPerCourse()
+        {
+            var courses = courseService.GetAll();
+            CourseView.PrintStudentsPerCourse(courses);
+
+        }
+        public void ReadAllTrainersPerCourse()
+        {
+            var courses = courseService.GetAll();
+            CourseView.PrintTrainersPerCourse(courses);
+        }
+        public void ReadAllAssignmentsPerCourse()
+        {
+            var courses = courseService.GetAll();
+            CourseView.PrintAssignmentsPerCourse(courses);
+
+        }
+        public void ReadAllAssignmentsPerStudentPerCourse()
+        {
+            var courses = courseService.GetAll();
+            CourseView.PrintAssignmentsPerStudentPerCourse(courses);
+        }
+        public void ReadStudentsToMoreThanOneCourse()
+        {
+            var students = studentService.GetAll();
+            StudentView.PrintStudentToMoreThanOneCourses(students);
+        }
+    }
+}
---
+++ Kitou_School_Project/Kitou_School_Project/Controller/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using Kitou_School_Project.RepositoryService;
+using Kitou_School_Project.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kitou_School_Project.Controller
+{
+    class StudentController
+    {
+        StudentRepository studentService = new StudentRepository();
+        public void ReadStudents()
+        {
+            var students = studentService.GetAll();
+            StudentView.PrintStudents(students);
+        }
+    }
+}
